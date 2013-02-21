3afe8eb9f442ad36b2b41aad99b4d52cc9689e03
dannyxu100 <dannyxu100@163.com>
2013-02-22 00:05:42 +0800

    新添流程管理总入口

---
+++ sys_workflow/index.php
@@ -0,0 +1,34 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+ <HEAD>
+  <TITLE>流程管理</TITLE>
+	<link rel="stylesheet" href="/css/base.css">
+	<style>
+		td{padding:3px;}
+		.must{color:#f00; font-weight:bold; padding-left:5px;}
+		
+		a.bt_menu{display:block;margin:3px;padding:5px; width:100px;overflow:hidden;border:1px solid #eee;background:#f5f5f5}
+	</style>
+ </HEAD>
+<BODY>
+<div>
+	<table class="tablesolid" style="width:100%">
+		<tr>
+			<td id="left_frame" style="vertical-align:top; width:120px;"><div id="menu_list"></div></td>
+			<td style="vertical-align:middle; width:10px; padding:0px;cursor:pointer;" onclick="slideleft()">
+				<img style="width:10px;height:50px;cursor:pointer;"/>
+			</td>
+			<td style="vertical-align:top;">
+				<iframe id="mainframe" src="" frameborder="0" style="width:100%;height:500px;" defaultHeight="600"></iframe>
+			</td>
+		</tr>
+	</table>
+	
+</div>
+</BODY>
+</HTML>
+
+
+<script type="text/javascript" src="/plugin/da/daLoader_source_1.1.js"></script>
+<script type="text/javascript" src="js/index.js"></script>
+
